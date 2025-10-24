c36d1d06a73d4706ade45aa5c23c0fa000606175
oateiva <oat@eiva.com>
2025-10-24 11:59:33 +0200

    Added metric depth anything

--- Module/Network/ModelSelector.py
+++ Module/Network/ModelSelector.py
@@ -4,9 +4,11 @@ import torch.nn as nn
 
 class ModelSelector:
     from Module.Network.DepthAnythingV2.dpt import DepthAnythingV2
+    from Module.Network.DepthAnythingV2.metric.dpt import DepthAnythingV2 as metric_DAV2
 
     _model_registry: dict[str, Type[nn.Module]] = {
         "DepthAnythingV2": DepthAnythingV2,
+        "MetricDepthAnythingV2": metric_DAV2
     }
 
     @staticmethod
